592876b5d32f2ef76784488acb0ecbc2412bc3e1
Naomi Risaka <13523122@std.stei.itb.ac.id>
2025-05-14 21:50:40 +0700

    feat: add necessary folders

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tucil3_13523019_13523122
+javac -d ../bin Main.java
+java -cp ../bin Main
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Selamat datang di Rush Hour Puzzle Solver!");
+    }
+}
